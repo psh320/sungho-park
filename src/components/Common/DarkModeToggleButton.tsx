08ab2ec5b826e486cd9c7a51a5ec626e7b144bbd
Sungho Park <43461389+psh320@users.noreply.github.com>
2023-03-11 17:57:43 +0800

    feat: add anition to darkmode toggle button

--- src/components/Common/DarkModeToggleButton.tsx
+++ src/components/Common/DarkModeToggleButton.tsx
@@ -4,8 +4,8 @@ export default function DarkModeToggleButton() {
   const { theme, setTheme } = useTheme();
   return (
     <button
-      className="inline-flex items-center  border-0 py-1 px-3  rounded
-      bg-gray-100 hover:bg-gray-200 dark:text-slate-400
+      className="inline-flex flex-row items-center justify-between  border-0 p-1 mx-3 rounded-xl w-14
+      bg-gray-200 hover:bg-gray-300 dark:text-slate-400
       dark:bg-slate-600 dark:hover:bg-gray-500
       focus:outline-none  hover:text-orange-400 dark:hover:text-yellow-300"
       type="button"
@@ -15,7 +15,7 @@ export default function DarkModeToggleButton() {
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
         fill="currentColor"
-        className="visible dark:invisible dark:w-0 dark:h-0 w-5 h-5"
+        className="w-5 h-5 visible dark:invisible"
       >
         <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z" />
       </svg>
@@ -23,7 +23,7 @@ export default function DarkModeToggleButton() {
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
         fill="currentColor"
-        className="invisible dark:w-5 dark:h-5 w-0 h-0 dark:visible"
+        className="w-5 h-5 dark:visible invisible"
       >
         <path
           fillRule="evenodd"
@@ -31,6 +31,7 @@ export default function DarkModeToggleButton() {
           clipRule="evenodd"
         />
       </svg>
+      <div className="w-5 h-5 bg-white absolute rounded-xl translate-x-7 dark:translate-x-0 duration-100" />
     </button>
   );
 }
