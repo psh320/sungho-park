dff181e93cb22825610d9fc967bd82175cb97346
Sungho Park <parksungho320@gmail.com>
2024-04-09 23:05:05 +0800

    rollback changes

--- src/pages/project/index.tsx
+++ src/pages/project/index.tsx
@@ -3,7 +3,6 @@ import NotionModal from "@/components/Project/ProjectPageModal";
 import { GetStaticProps } from "next";
 import { useEffect, useState } from "react";
 import { TOKEN, DATABASE_ID } from "../../config/index";
-import { projectsList } from "@/public/static/data";
 
 export type ProjectType = {
   id: string;
@@ -20,7 +19,10 @@ export type ProjectType = {
   site: string | null;
 };
 
-export default function Project() {
+type Props = {
+  projects: ProjectType[];
+};
+export default function Project({ projects }: Props) {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [pageId, setPageId] = useState("");
   const [modalProject, setModalProject] = useState<ProjectType | null>(null);
@@ -29,7 +31,7 @@ export default function Project() {
     <div className="flex flex-col min-h-screen justify-center items-center px-5 pt-24 mb-12">
       <h1 className="font-bold text-5xl">Project</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 py-10 m-6 gap-8 items-center justify-center max-w-6xl">
-        {projectsList.map((project, key) => (
+        {projects.map((project, key) => (
           <ProjectItems
             key={key}
             project={project}
@@ -49,4 +51,60 @@ export default function Project() {
       />
     </div>
   );
+}
+
+//called at build time
+export async function getServerSideProps(context: GetStaticProps) {
+  const options = {
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      "Notion-Version": "2022-06-28",
+      "content-type": "application/json",
+      Authorization: `Bearer ${TOKEN}`,
+    },
+    body: JSON.stringify({
+      page_size: 100,
+      sorts: [
+        {
+          property: "Duration",
+          direction: "descending",
+        },
+      ],
+    }),
+  };
+
+  const res = await fetch(
+    `https://api.notion.com/v1/databases/${DATABASE_ID}/query`,
+    options
+  );
+  const projects = await res.json();
+  const parsedData = parseDatabase(projects.results);
+  return {
+    props: { projects: parsedData }, // will be passed to the page component as props
+  };
+}
+
+function parseDatabase(data: any): ProjectType[] {
+  return data.map((project: any) => ({
+    id: project.id,
+    title: project.properties.Name.title[0].plain_text,
+    duration: project.properties.Duration.date,
+    github: project.properties.Github.url,
+    demo: project.properties.Demo.url,
+    site: project.properties.Site.url,
+    ios: project.properties.ios.url,
+    android: project.properties.android.url,
+    tech: project.properties.Tech.multi_select,
+    description:
+      project.properties.Description.rich_text.length === 0
+        ? null
+        : project.properties.Description.rich_text[0].plain_text,
+    projectURL: project.url,
+    coverImage: project.cover
+      ? project.cover.external
+        ? project.cover.external.url
+        : project.cover.file.url
+      : null,
+  }));
 }
